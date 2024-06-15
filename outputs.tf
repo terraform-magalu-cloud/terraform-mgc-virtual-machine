f9ae629107191a460039dec617cae52f6510da6c
Antonio Novaes <antonionovaesjr@gmail.com>
2024-06-15 18:51:40 -0300

    fix: The given key does not identify an element in this collection

--- outputs.tf
+++ outputs.tf
@@ -1,55 +1,66 @@
 output "created_at" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "Timestamp of when the virtual machine was created"
-  value       = resource.mgc_virtual-machine_instances.this[0].created_at
+  value       = try(resource.mgc_virtual-machine_instances.this[0].created_at, null)
 }
 
 output "instance_id" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "ID of the created virtual machine"
-  value       = resource.mgc_virtual-machine_instances.this[0].id
+  value       = try(resource.mgc_virtual-machine_instances.this[0].id, null)
 }
 
 output "status" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "Status of the virtual machine created, running, stopped, etc."
-  value       = resource.mgc_virtual-machine_instances.this[0].status
+  value       = try(resource.mgc_virtual-machine_instances.this[0].status, null)
 }
 
 output "current_ssh_key_name" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "Name of the ssh key used"
-  value       = resource.mgc_virtual-machine_instances.this[0].current_ssh_key_name
+  value       = try(resource.mgc_virtual-machine_instances.this[0].current_ssh_key_name, null)
 }
 
 output "updated_at" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "Timestamp of last modified virtual machine"
-  value       = resource.mgc_virtual-machine_instances.this[0].updated_at
+  value       = try(resource.mgc_virtual-machine_instances.this[0].updated_at, null)
 }
 
 output "current_machine_type_id" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "Instance type ID used"
-  value       = resource.mgc_virtual-machine_instances.this[0].current_machine_type.id
+  value       = try(resource.mgc_virtual-machine_instances.this[0].current_machine_type.id, null)
 }
 
 
 output "current_machine_type_name" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "Name of the instance type used"
-  value       = resource.mgc_virtual-machine_instances.this[0].current_machine_type.name
+  value       = try(resource.mgc_virtual-machine_instances.this[0].current_machine_type.name, null)
 }
 
 output "current_name" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "Virtual machine name"
-  value       = resource.mgc_virtual-machine_instances.this[0].current_name
+  value       = try(resource.mgc_virtual-machine_instances.this[0].current_name, null)
 }
 
 output "current_image_id" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "ID of the image used in the virtual machine"
-  value       = resource.mgc_virtual-machine_instances.this[0].current_image.id
+  value       = try(resource.mgc_virtual-machine_instances.this[0].current_image.id, null)
 }
 
 output "associate_public_ip" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "Boolean value, whether there is a public IP or not: true or false"
-  value       = resource.mgc_virtual-machine_instances.this[0].network.associate_public_ip
+  value       = try(resource.mgc_virtual-machine_instances.this[0].network.associate_public_ip, null)
 }
 
 output "vpc" {
+  depends_on  = [mgc_virtual-machine_instances.this]
   description = "VPC being used by the virtual machine"
-  value       = resource.mgc_virtual-machine_instances.this[0].network.vpc
+  value       = try(resource.mgc_virtual-machine_instances.this[0].network.vpc, null)
 }
