08f09c0f360804a03c28d3de88b80d298a6f2a63
Antonio Novaes <antonionovaesjr@gmail.com>
2024-06-23 20:03:42 -0300

    feat: improv example

--- examples/main.tf
+++ examples/main.tf
@@ -1,22 +1,65 @@
 provider "mgc" {
+  alias  = "ne"
   region = "br-ne1"
 }
 
-module "instance" {
+provider "mgc" {
+  alias  = "se"
+  region = "br-se1"
+}
+
+resource "random_string" "sufix_ne" {
+  length  = 6
+  special = false
+}
+
+resource "random_string" "sufix_se" {
+  length  = 6
+  special = false
+}
+
+
+module "instance_ne" {
+  providers = {
+    mgc = mgc.ne
+  }
   source       = "../"
   create       = true
-  name         = var.name
+  name         = "${var.name}-ne-${random_string.sufix_ne.id}"
   ssh_key_name = "key-example"
   additional_disk = {
     vdb = {
       name = "opt"
       size = 10
-      type = "cloud_nvme1k"
+      type = "cloud_nvme5k"
     },
     vdc = {
       name = "tmp"
       size = 10
-      type = "cloud_nvme1k"
+      type = "cloud_nvme5k"
+    }
+  }
+  user_data = file("user-data.sh")
+}
+
+module "instance_se" {
+  providers = {
+    mgc = mgc.se
+  }
+  source       = "../"
+  create       = true
+  name         = "${var.name}-se-${random_string.sufix_se.id}"
+  ssh_key_name = "key-example"
+  additional_disk = {
+    vdb = {
+      name = "opt"
+      size = 10
+      type = "cloud_nvme5k"
+    },
+    vdc = {
+      name = "tmp"
+      size = 10
+      type = "cloud_nvme5k"
     }
   }
   user_data = file("user-data.sh")
--- examples/outputs.tf
+++ examples/outputs.tf
@@ -1,10 +1,21 @@
-output "id" {
+output "instance_ne_id" {
   description = "ID of the macine"
-  value       = module.instance.instance_id
+  value       = module.instance_ne.instance_id
 }
 
-output "associate_public_ip" {
+output "instance_ne_associate_public_ip" {
   description = "If machine has ip public, boolean"
-  value       = module.instance.associate_public_ip
+  value       = module.instance_ne.associate_public_ip
 
+}
+
+
+output "instance_se_id" {
+  description = "ID of the macine"
+  value       = module.instance_se.instance_id
+}
+
+output "instance_se_associate_public_ip" {
+  description = "If machine has ip public, boolean"
+  value       = module.instance_se.associate_public_ip
 }
